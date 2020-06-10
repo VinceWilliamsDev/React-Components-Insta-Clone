dd85ba1ec01dd537a1081f66424d6bb7411677fb
Vince Williams <williamsvm20@gmail.com>
2020-06-09 23:55:02 -0400

    Insta works

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -5,12 +5,24 @@ import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
+  console.log(props)
   // Add state for the comments
+  const [comments, addComment] = useState(props.comments)
+
+  const submitComment = () => addComment(comments.push)
+  const changeComment = () => {
+    addComment(comments.pop)
+    addComment(comments.push)
+  }
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {comments.map(comment => {
+        // console.log(comment)
+        return <Comment comment={comment} />
+      })}
+      <CommentInput /*submitComment={submitComment} changeComment={changeComment}*/ />
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import postData from "../../dummy-data"
 
 const PostsPage = () => {
   // set up state for your data
+  const [posts, setPost] = useState(postData)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {posts.map(post => {
+        return <Post key={post.timestamp} post={post} />
+      })}
     </div>
   );
 };
